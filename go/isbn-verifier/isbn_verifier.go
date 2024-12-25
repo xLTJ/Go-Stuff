2128da3b42045d9a85b2d9c048755f2773ac1119
xLTJ <lucilorate@proton.me>
2024-12-25 16:27:31 +0100

    added benchmark result to isbn verifier

--- go/isbn-verifier/isbn_verifier.go
+++ go/isbn-verifier/isbn_verifier.go
@@ -1,6 +1,6 @@
 package isbn
 
-// IsValidISBN checks if a string is a valid ISBN (178 ns/op)
+// IsValidISBN checks if a string is a valid ISBN
 func IsValidISBN(isbn string) bool {
 	comparisonNumber := 0
 	digit := 0
